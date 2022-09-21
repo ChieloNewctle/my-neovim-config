0885a58dbf79f6909c439f2a7bb8761eea661754
Chielo Newctle <ChieloNewctle@Yandex.com>
2022-09-21 21:49:19 +0800

    change lsp config

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -11,20 +11,11 @@ lsp_installer.on_server_ready(function(server)
 		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
+    local has_user_settings, user_settings = pcall(require, "user.lsp.settings." .. server.name)
 
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
-
-	 if server.name == "pyright" then
-	 	local pyright_opts = require("user.lsp.settings.pyright")
-	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	 end
+    if has_user_settings then
+        opts = vim.tbl_deep_extend("force", user_settings, opts)
+    end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
