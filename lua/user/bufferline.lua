a614adf4900c3d9e982e25faeb45836a8b2b29bb
Chielo Newctle <ChieloNewctle@Yandex.com>
2022-09-23 23:51:57 +0800

    update settings

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -7,15 +7,15 @@ bufferline.setup {
   options = {
     numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
     close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
-    right_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+    right_mouse_command = '', -- can be a string | function, see "Mouse actions"
     left_mouse_command = 'buffer %d', -- can be a string | function, see "Mouse actions"
     middle_mouse_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
     max_name_length = 30,
     max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
     tab_size = 21,
-    diagnostics = "coc", -- | "nvim_lsp" | "coc",
+    diagnostics = 'coc', -- | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
-    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+    offsets = { { filetype = 'NvimTree', text = '', padding = 1 } },
     color_icons = true,
     show_buffer_icons = true,
     show_buffer_close_icons = true,
@@ -26,7 +26,7 @@ bufferline.setup {
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+    separator_style = 'thin', -- | "thick" | "thin" | { 'any', 'any' },
     enforce_regular_tabs = true,
     always_show_bufferline = true,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,9 +1,9 @@
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
+local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+local config_status_ok, nvim_tree_config = pcall(require, 'nvim-tree.config')
 if not config_status_ok then
   return
 end
@@ -13,6 +13,7 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
   sync_root_with_cwd = true,
   hijack_cursor = true,
+  open_on_tab = false,
   diagnostics = {
     enable = true,
   },
@@ -26,11 +27,11 @@ nvim_tree.setup {
     timeout = 500,
   },
   view = {
-    side = "left",
+    side = 'left',
     mappings = {
       custom_only = false,
       list = {
-        { key = "h", cb = tree_cb "close_node" },
+        { key = 'h', cb = tree_cb 'close_node' },
       },
     },
   },
