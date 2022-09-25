6ff2ec90eb3a9bc83b46155150870ba57d5a9d9d
Chielo Newctle <ChieloNewctle@Yandex.com>
2022-09-25 21:54:07 +0800

    add support to project-wise settings

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,5 +1,20 @@
 -- autocmd! remove all autocommands, if entered under a group it will clear that group
 vim.cmd [[
+  function! SourceProjectConfig() abort
+    let l:projectfile = findfile('.vim/local.vim', expand('%:p').';')
+    if filereadable(l:projectfile)
+      silent execute 'source' l:projectfile
+    endif
+  endfunction
+
+  augroup _project_config
+    autocmd!
+    autocmd BufRead,BufNewFile * call SourceProjectConfig()
+    if has('nvim')
+      autocmd DirChanged * call SourceProjectConfig()
+    endif
+  augroup end
+
   augroup _git
     autocmd!
     autocmd FileType gitcommit setlocal wrap
