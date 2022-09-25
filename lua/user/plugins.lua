71a80e8a93bb5938c54e3f70dd374c6b97a1c0fc
Chielo Newctle <ChieloNewctle@Yandex.com>
2022-09-25 21:59:08 +0800

    add todo comments

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -105,6 +105,9 @@ return packer.startup(function(use)
   -- Whichkey
   use 'folke/which-key.nvim'
 
+  -- Todo comments
+  use { 'folke/todo-comments.nvim', requires = 'nvim-lua/plenary.nvim' }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
